776f3b05e4efbabe90b40b71d8a32a84f3859e92
Krlozz <k_arlos210@hotmail.com>
2018-10-29 18:04:48 -0500

    ejercicio recorrer arreglo y transformar a promesa

--- 01-javascript/07-promesas.js
+++ 01-javascript/07-promesas.js
@@ -57,3 +57,113 @@ nuevaPromesa(nombre)
             console.log('Catch',error);
         }
     );
+
+
+
+
+//['A','B','C']
+//0-A.txt  'A'
+//1-B.txt  'B'
+//2-C.txt  'C'
+
+const respuesta={
+    nombreArchivo:'',
+    contenidoArchivo:'',
+    error:'',
+};
+[respuesta,respuesta,respuesta,respuesta]
+function ejercicio(arreglotrings,callback){
+    const respuestas=[];
+    arreglotrings
+        .forEach(
+            (string, indice)=>{
+                const nombreArchivo=`${indice}-${string}.txt`;
+                const contenido = string;
+                fs.writeFile(
+                    nombreArchivo,
+                    contenido,
+                    (err)=>{
+                        const respuesta={
+                            nombreArchivo:nombreArchivo,
+                            contenidoArchivo:contenido,
+                            error:err,
+                        };
+                        respuestas.push(respuesta);
+                        const estaCompletoArreglo = respuetas.length===arreglotrings.length;
+                        if(estaCompletoArreglo){
+                            callback(respuestas);
+                        }
+                    }
+                )
+
+            }
+        )
+}
+
+ejercicio(
+    ['A','B','C'],
+    (respuetaEjercicio)=>{
+        console.log(respuetaEjercicio);
+    }
+);
+
+
+
+
+// ejercicio en clase
+
+const appendFile = (nombreArchivo, contenido) => {
+    return new Promise(
+        (resolve, reject) => {
+            fs.readFile(
+                nombreArchivo,
+                'utf-8',
+                (error, contenidoLeidoDelArchivo) => {
+                    if (error) {
+                        fs.writeFile(
+                            nombreArchivo,
+                            contenido,
+                            (err) => {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    // Devolver el contenido
+                                    resolve(contenido)
+                                }
+                            }
+                        );
+                    } else {
+                        fs.writeFile(
+                            nombreArchivo,
+                            contenidoLeidoDelArchivo + contenido,
+                            (err) => {
+                                if (err) {
+                                    reject(err);
+                                } else {
+                                    // Devolver el contenido
+                                    resolve(contenidoLeidoDelArchivo + contenido)
+                                }
+                            }
+                        )
+                    }
+                }
+            );
+        }
+    )
+
+}
+
+
+appendFile(nombre)
+    .then(
+        (contenido) => {
+            console.log(contenido);
+            return nuevaPromesaEscritura('07-ejemplo2.txt', contenido + 'Adios amigos');
+        }
+    )
+    .catch(
+        (error) => {
+            console.log('Catch',error);
+        }
+    );
+
