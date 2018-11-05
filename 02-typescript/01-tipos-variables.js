69694216fc980c835c1ac18a6c9cfc01c05651ba
Krlozz <k_arlos210@hotmail.com>
2018-11-05 17:20:46 -0500

    transpilado en es2017

--- 02-typescript/01-tipos-variables.js
+++ 02-typescript/01-tipos-variables.js
@@ -1,24 +1,24 @@
 // 01-tipo-variables.ts
-var edad = 13;
+let edad = 13;
 edad = 25;
 edad = 'Nombre';
-var variableLoca = '';
+let variableLoca = '';
 variableLoca = false;
-var nombre = 'Carlos';
+let nombre = 'Carlos';
 // duck typing -->  // además no es necesario poner le tipado el lenguaje ya sabe la variable, typescript ya deduce cual es el tipo de variable
 //nombre = 13;
-var casado = false;
+let casado = false;
 casado = true;
 casado = false;
 casado = null;
 casado = undefined;
-var carlos = {
+let carlos = {
     nombre: 'Carlos',
     apellido: 'Ayala'
 };
 console.log(carlos); // undefined
-var numeros = [1, 2, 3, 4];
-var fechaNacimiento = new Date();
+let numeros = [1, 2, 3, 4];
+let fechaNacimiento = new Date();
 /*
 let promesa:Promise<number> = () => { return new Promise((resolve,reject)=> {
     resolve(1);
@@ -27,63 +27,44 @@ let promesa:Promise<number> = () => { return new Promise((resolve,reject)=> {
 };
 */
 function saludar(nombre, // parametros requeridos
-apellido) {
-    var otrosNombres = []; /// parametros infinitos
-    for (var _i = 2 /// parametros infinitos
-    ; _i < arguments.length /// parametros infinitos
-    ; _i++ /// parametros infinitos
-    ) {
-        otrosNombres[_i - 2] = arguments[_i]; /// parametros infinitos
-    }
+apellido, // parametros opcionales
+...otrosNombres /// parametros infinitos
+) {
     return 'Hola';
 }
-var respuestaSaludar = saludar('Carlos', 'Ayala', '', '', '', '');
+let respuestaSaludar = saludar('Carlos', 'Ayala', '', '', '', '');
 // casteo de datos
 function saludar2(nombre, // parametros requeridos
-apellido) {
-    var otrosNombres = []; /// parametros infinitos
-    for (var _i = 2 /// parametros infinitos
-    ; _i < arguments.length /// parametros infinitos
-    ; _i++ /// parametros infinitos
-    ) {
-        otrosNombres[_i - 2] = arguments[_i]; /// parametros infinitos
-    }
+apellido, // parametros opcionales
+...otrosNombres /// parametros infinitos
+) {
     return 'Hola';
 }
-var respuestaSaludar2 = saludar2('Carlos', 'Ayala', '', '', '', '');
+let respuestaSaludar2 = saludar2('Carlos', 'Ayala', '', '', '', '');
 respuestaSaludar2 = 1;
 // casteo de uno o el otro
 function saludar3(nombre, // parametros requeridos
-apellido) {
-    var otrosNombres = []; /// parametros infinitos
-    for (var _i = 2 /// parametros infinitos
-    ; _i < arguments.length /// parametros infinitos
-    ; _i++ /// parametros infinitos
-    ) {
-        otrosNombres[_i - 2] = arguments[_i]; /// parametros infinitos
-    }
+apellido, // parametros opcionales
+...otrosNombres /// parametros infinitos
+) {
     return 'Hola';
 }
-var respuestaSaludar3 = saludar2('Carlos', 'Ayala', '', '', '', '');
+let respuestaSaludar3 = saludar2('Carlos', 'Ayala', '', '', '', '');
 respuestaSaludar3 = 1;
-var saludo = function (nombre) {
+const saludo = (nombre) => {
     return 1;
 };
-var Usuario = /** @class */ (function () {
-    function Usuario() {
+class Usuario {
+    constructor() {
     }
-    Usuario.prototype.saludar = function (nombre) {
+    saludar(nombre) {
         return nombre;
-    };
-    return Usuario;
-}());
-var carlosInstancia = new Usuario();
-var UsuarioDummy = /** @class */ (function () {
-    function UsuarioDummy() {
     }
-    return UsuarioDummy;
-}());
-var carlos2 = {
+}
+const carlosInstancia = new Usuario();
+class UsuarioDummy {
+}
+const carlos2 = {
     nombre: 'Carlos',
-    apellido: 'Ayala'
+    apellido: 'Ayala',
 };
