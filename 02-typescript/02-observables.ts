8f059596ace7a622465a9633578067c99366b9e6
Krlozz <k_arlos210@hotmail.com>
2018-11-09 16:39:35 -0500

    observable
    uso de operadores en observables
    
    función pipe

--- 02-typescript/02-observables.ts
+++ 02-typescript/02-observables.ts
@@ -2,13 +2,34 @@
 
 declare var require: any;
 const rxjs = require('rxjs');
+const map = require('rxjs/operators').map;
 
-const numeros$  = rxjs.of(1,2,3,4,5,6);
+const numeros$  = rxjs.of(
+    1,
+    "Carlos",
+    true,
+    {nombre: 'Carlos'},
+    [1,2,3],
+    new Date()
+);
 
 console.log(numeros$);
 
 
 numeros$
+    .pipe(
+        map(
+            (valorActual) => {
+                return {data: valorActual};
+            }
+        )
+    )
+    /*
+    .pipe()
+    .pipe()
+    .pipe()
+    .pipe()
+    .pipe()*/
     .subscribe(
         (ok) => {
             console.log('En ok',ok);
