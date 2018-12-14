7a5d50d7a17da2906e2cc6238aa9f97113ce63fe
Krlozz Ayala <k_arlos210@hotmail.com>
2018-12-14 16:46:26 -0500

    funcionalidad de servicio

--- 03-Angular/videos/src/app/rutas/ruta-gestion-usuarios/ruta-gestion-usuarios.component.ts
+++ 03-Angular/videos/src/app/rutas/ruta-gestion-usuarios/ruta-gestion-usuarios.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {UsuarioServiceService} from "../../servicios/usuario-service.service";
 
 @Component({
   selector: 'app-ruta-gestion-usuarios',
@@ -7,38 +8,25 @@ import {Component, OnInit} from '@angular/core';
 })
 export class RutaGestionUsuariosComponent implements OnInit {
 
-  usuarios: Usuario[] = [
-    {
-      id: 1,
-      nombre: 'Adrian'
-    },
-    {
-      id: 2,
-      nombre: 'Vicente'
-    }
-  ];
+  usuarios = [];
 
+  // Inyección de Dependencias
+  constructor(
+    private readonly _usuarioService:UsuarioServiceService
+  ) {
 
-  constructor() {
   }
 
   ngOnInit() {
+    this._usuarioService.usuarios;
   }
 
-  hola() {
-    return 'Hola';
-  }
+  // hola() {
+  //   return 'Hola';
+  // }
 
-  imprimir(usuario: Usuario) {
-    console.log('Imprimir', usuario);
-
-    const indiceUsuarioAEliminar = this.usuarios
-      .findIndex(
-        (usuarioABuscar) => {
-          return usuarioABuscar.id === usuario.id;
-        });
-
-    this.usuarios.splice(indiceUsuarioAEliminar, 1);
+  eliminar(usuario) {
+    this._usuarioService.eliminar(usuario.id);
 
 
   }
--- 03-Angular/videos/src/app/servicios/usuario-service.service.ts
+++ 03-Angular/videos/src/app/servicios/usuario-service.service.ts
@@ -5,5 +5,60 @@ import { Injectable } from '@angular/core';
 })
 export class UsuarioServiceService {
 
+  usuarios: Usuario[] = [
+    {
+      id: 1,
+      nombre: 'Adrian'
+    },
+    {
+      id: 2,
+      nombre: 'Vicente'
+    }
+  ];
+
+  registroActual = 3;
+
   constructor() { }
+
+  crear(nuevoUsuario:Usuario): Usuario {
+    nuevoUsuario.id=this.registroActual;
+    this.usuario.push(nuevoUsuario);
+    this.registroActual++;
+    return nuevoUsuario
+  }
+
+  eliminar(id:number) {
+    const indiceUsuario = this.usuarios
+      .findIndex(
+        (usuario) => {
+          return usuario.id === id;
+        }
+      );
+    const usuarioBorrado = JSON.parse(
+      JSON.stringify(this.usuarios[indiceUsuario])
+    );
+    this.usuarios.splice(indiceUsuario, 1);
+    return usuarioBorrado;
+  }
+
+  actualizar(id:number, usuarioActualizado:Usuario) {
+    const indiceUsuario = this.usuarios
+      .findIndex(
+        (usuario) => {
+          return usuario.id === id;
+        }
+      );
+
+    this.usuarios[indiceUsuario] = usuarioActualizado
+    return usuarioActualizado;
+  }
+
+}
+
+
+
+
+export interface Usuario {
+  nombre?: string;
+  id?: number;
 }
