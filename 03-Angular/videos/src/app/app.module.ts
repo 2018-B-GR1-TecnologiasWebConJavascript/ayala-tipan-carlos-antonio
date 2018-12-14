560a2ee3dccc77dd39a87f21c25060115042d185
Krlozz Ayala <k_arlos210@hotmail.com>
2018-12-14 08:33:02 -0500

    creadas todos los componentes rutas

--- 03-Angular/videos/src/app/app.module.ts
+++ 03-Angular/videos/src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { RutaInicioComponent } from './rutas/ruta-inicio/ruta-inicio.component';
+import { RutaPerfilComponent } from './rutas/ruta-perfil/ruta-perfil.component';
+import { Ruta404Component } from './rutas/ruta404/ruta404.component';
+import { RutaLoginComponent } from './rutas/ruta-login/ruta-login.component';
+import { RutaMenuComponent } from './rutas/ruta-menu/ruta-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    RutaInicioComponent
+    RutaInicioComponent,
+    RutaPerfilComponent,
+    Ruta404Component,
+    RutaLoginComponent,
+    RutaMenuComponent
   ],          // Web Components
   imports: [
     BrowserModule,
