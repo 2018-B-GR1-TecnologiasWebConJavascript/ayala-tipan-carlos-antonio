e78cb785f25e778ec962f86766694565072f81cd
Krlozz Ayala <k_arlos210@hotmail.com>
2018-12-14 08:23:38 -0500

    componente ruta inicio y ng build

--- 03-Angular/videos/src/app/app.module.ts
+++ 03-Angular/videos/src/app/app.module.ts
@@ -3,16 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RutaInicioComponent } from './rutas/ruta-inicio/ruta-inicio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
-  ],
+    AppComponent,
+    RutaInicioComponent
+  ],          // Web Components
   imports: [
     BrowserModule,
     AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  ],               // Módulos
+  providers: [],                // Servicios
+  bootstrap: [AppComponent]     // Componente principal
 })
 export class AppModule { }
+
+
+
+
+
+/*
+-> Componente Principal
+  <router-outlet></router-outlet>
+    -> *Inicio  -> inicio
+    -> *Login
+    -> *Perfil
+    -> *Menu -> menu/
+     <router-outlet></router-outlet>
+        -> Gestion Usuarios ->
+            <router-outlet></router-outlet>
+            menu/gestion-usuarios/crear-usuarios
+            -> Crear usuarios
+            -> Actualizar Usuarios
+        -> Gestion Productos
+            -> Crear productos
+            -> Actualizar productos
+    -> *404 -> Not Found
+*
+* */
